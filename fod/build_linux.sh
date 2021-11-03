58115a1374411a337ff7945a6deb4eceae7b0fd0
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-11-04 00:25:39 +0100

    fix

--- fod/build_linux.sh
+++ fod/build_linux.sh
@@ -23,13 +23,14 @@ gcc -ofod_milomqtt fod.c flecs.c milo/milomqtt.c \
 gcc -ofod_probe fod.c flecs.c probe/probe.c \
 -DIMPLEMENT_PROBE \
 -I. \
+-DNNG_STATIC_LIB \
+-lnng \
 -latomic \
 -llapacke \
 -llapack \
 -lblas \
 -lm \
--lpthread \
--lmosquitto
+-lpthread
 
 cp -f fod_probe /usr/local/bin/fod_probe
 cp -f fod_milomqtt /usr/local/bin/fod_milomqtt
