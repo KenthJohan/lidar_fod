301030b3faa36a8d24f7d3dc3222786390e97352
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-11-03 15:42:29 +0100

    test

--- test2/test.c
+++ test2/test.c
@@ -24,16 +24,86 @@ static void csc_crossos_enable_ansi_color ()
 #endif
 }
 
+
+ECS_STRUCT(Position, {
+float x;
+float y;
+});
+
+ECS_STRUCT(Velocity, {
+float x;
+float y;
+});
+
+
+
+void Move(ecs_iter_t *it) {
+	Position *p = ecs_term(it, Position, 1);
+	const Velocity *v = ecs_term(it, const Velocity, 2);
+
+	for (int i = 0; i < it->count; i ++) {
+		p[i].x += v[i].x;
+		p[i].y += v[i].y;
+	}
+}
+
+void PrintPosition(ecs_iter_t *it) {
+	const Position *p = ecs_term(it, const Position, 1);
+
+	for (int i = 0; i < it->count; i ++) {
+		printf("%s: {%f, %f}\n", ecs_get_name(it->world, it->entities[i]),
+		p[i].x, p[i].y);
+	}
+}
+
+
+
 int main (int argc, char const * argv[])
 {
 	csc_crossos_enable_ansi_color();
 	ecs_world_t *world = ecs_init_w_args(1, (char*[]){
 	"rest_test", NULL // Application name, optional
 	});
+	ecs_set(world, EcsWorld, EcsRest, {0});
 	ecs_log_set_level(2);
-	ecs_trace("ecs_app_run '%s:%s'", "rest_test", "true");
+	// This registers the component with reflection data
+	ECS_META_COMPONENT(world, Position);
+	ECS_META_COMPONENT(world, Velocity);
+	//ecs_trace("ecs_app_run '%s:%s'", "rest_test", "true");
+
+
+	ECS_SYSTEM(world, Move, EcsOnUpdate, Position, [in] Velocity);
+	ECS_SYSTEM(world, PrintPosition, EcsPostUpdate, [in] Position);
+
+	Position value = {10, 20};
+	char *json = ecs_ptr_to_json(world, ecs_id(Position), &value);
+	printf("%s\n", json); // {"x": 10.0, "y": 20.0}
+	ecs_os_free(json);
+
+
+	ecs_entity_t e1 = ecs_new_entity(world, "e1");
+	ecs_set(world, e1, Position, {10, 20});
+	ecs_set(world, e1, Velocity, {1, 2});
+
+	ecs_entity_t e2 = ecs_new_entity(world, "e2");
+	ecs_set(world, e2, Position, {10, 20});
+	ecs_set(world, e2, Velocity, {3, 4});
+
+
+	/*
 	return ecs_app_run(world, &(ecs_app_desc_t) {
 	.target_fps = 60,
 	.enable_rest = true
 	});
+	*/
+
+
+	int c = getchar();
+	while(c != 'q')
+	{
+		ecs_progress(world, 0.0f);
+		c = getchar();
+	}
+
+	return ecs_fini(world);
 }
